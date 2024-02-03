30733aece115cb99cb8289a403e9066e04a864b5
WaiYanMoeMyintt <wymoemyint4523@gmail.com>
2024-02-03 20:58:09 +0630

    Create SideMenu compoennts

--- src/components/SideContent.jsx
+++ src/components/SideContent.jsx
@@ -4,13 +4,30 @@ import Today from "../pages/Today.jsx";
 import Travel from "../pages/Travel.jsx";
 import Meeting from "../pages/Meeting.jsx";
 import Overview from "../pages/Overview.jsx";
+import SideMenu from './SideMenu.jsx';
 import { Route, Routes } from 'react-router-dom';
 
 const SideContent = () => {
+
+    const time =  new Date().getHours();
+    let message = "";
+    if(time >= 18){
+        message =  "Good Night Miracle";
+    }
+    else if(time <  12){
+      message ="Good Morning Miracle"
+    }
+    else if(time <= 15){
+      message = "Good Afternoon Miracle"
+    }
+    else if(time <= 17){
+      message = "Good Evening Miracle";
+    }
+
   return (
     <div className="sidecontent_container">
           <div className="sidecontent_content py-4 px-4">
-              <h2>Fucker</h2>
+               <SideMenu message = {message}/>
           </div>
           <Routes>
               <Route path = {"/Overview"} element = {<Overview />} />
