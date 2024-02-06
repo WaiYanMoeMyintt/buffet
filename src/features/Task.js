b68eadde520760b2a9d2cb59f7412242202c58e2
WaiYanMoeMyintt <wymoemyint4523@gmail.com>
2024-02-06 19:54:02 +0630

    Create Task Redux Component:

---
+++ src/features/Task.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const createTask = createSlice({
+      name : "Task",
+      initialState:{
+        value:[{
+            id: Date.now(),
+            name:"Create a Plan",
+            description:"for social media marketing",
+            complete:false
+        }]
+      },
+      reducers:{
+          create:(state,action)=>{
+              state.value.unshift(action.payload);
+          },
+          deleteTask:(state,action)=>{
+              state.value =  state.value.filter((items)=>{
+                    items.id !== action.payload.type.id;
+               })
+          }
+      }
+
+});
+
+export const {create, deleteTask} =  createTask.actions;
+export default createTask.reducer;
